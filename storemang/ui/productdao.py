f13803c81ce0c623b2a13f608f1519d2eda2847e
Mathews-Gigi <114875469+Mathews-Gigi@users.noreply.github.com>
2023-08-19 09:42:45 +0000

    Sat, Aug 19, 2023, 3:12 PM +05:30

--- storemang/ui/productdao.py
+++ storemang/ui/productdao.py
@@ -2,4 +2,13 @@ from flask import flask ,request , jsonify
 
 app = flask(__name__)
 
-@app.
+@app.route('/hello')
+
+def hello():
+        return"Hello,How are you"
+
+if __name__="__main__":
+    print("starting python flask server for a gocery store management system")
+
+app.run(port=5000)
+
